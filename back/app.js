bf5d57c479c0e1d4956cf29c12f3010d0b5dadcb
Esko <esko.takku@gmail.com>
2017-11-11 15:56:10 +0200

    Testiä

--- back/app.js
+++ back/app.js
@@ -29,6 +29,8 @@ Tallenna kantaan.
 >>>>
 
 
+Hae valojen tilat kantaan
+
 
 
 
@@ -56,14 +58,32 @@ db.once('open', function() {
 
 var lighschema = mongoose.Schema({
     coordinates: [Number], // geometry.coordinates
-    laite: String, // "tre" + properties.ILMAISIN_ID
-    etaisyys: Number, // properties.ETAISYYS
-    tunnus: String // properties.TUNNUS
+    device: String, // "tre" + properties.ILMAISIN_ID
+    distance: Number, // properties.ETAISYYS
+    detector: String // properties.TUNNUS
 });
 
 var Light = mongoose.model('Light', lighschema);
 
+var congestionschema = mongoose.Schema({
+  hostId : String,
+  device: String,
+  detector: String,
+  tsPeriodEnd: String,
+  redPeriod: Number,
+  queueLength: Number,
+  unitQueueLength: String,
+  vehicleCount: Number,
+  unitVehicleCount: String,
+  maxWaitTime: Number,
+  avgWaitTime: Number,
+  unitWaitTime: String,
+  reliabValue: Number,
+  unitReliab: String
+});
 
+
+var Congestion = mongoose.model('Congestion', congestionschema);
 
 console.log("test");
 app.use(bodyParser.json());
@@ -107,6 +127,13 @@ app.use(function(req, res, next) {
 // });
 // });
 
+app.get('/congestion', function(req, res) {
+  Congestion.find(function(err, response) {
+    if (err) return console.error(err);
+    res.send(response);
+  });
+});
+
 
 app.get('/lights', function(req, res) {
   Light.find(function(err, response) {
@@ -115,13 +142,130 @@ app.get('/lights', function(req, res) {
   });
 });
 
+// http://193.185.142.46/TrafficlightdataService/rest/get-traffic-queue-length-and-wait-time?device=tre309&historyMinutes=3
+
+app.get('/HaeData', function(req, res) {
+
+  var request = require("request")
+
+  var url = "http://193.185.142.46/TrafficlightdataService/rest/get-traffic-queue-length-and-wait-time?device=tre309&historyMinutes=10"
+
+  donearray = [];
+
+  Light.find(function(err, lights) {
+    if (err) return console.error(err);
+    
+    for (index = 0; index < lights.length; ++index) {
+
+       u = lights[index]
+       //if (donearray.indexOf(u.device) > -1){
+        if (donearray.length > 0){
+         continue
+       }
+
+       console.log('Waiting');
+       var sleep = require("sleep");
+       sleep.sleep(1);
+
+      var url = "http://193.185.142.46/TrafficlightdataService/rest/get-traffic-queue-length-and-wait-time?"+
+      "device="+u.device+"&"+
+      "historyMinutes=2";
+      console.log(url);
+      http.get(url, function(rs){
+          var body = '';
+          console.log("Loopdi");
+          rs.on('data', function(chunk){
+              body += chunk;
+          });
+
+          rs.on('end', function(){
+              try {
+                console.log(body);
+                var resp = JSON.parse(body);
+                donedetector = [];
+                for (index = 0; index < resp.results.length; ++index) {
+                  if (donedetector.indexOf(resp.results[index].detector) > -1){
+                    continue;
+                  }
+                  donedetector.push(resp.results[index].detector);
+                  var men = false;
+                  try {
+                    var newItem = new Congestion({
+                      coordinates:  resp.results[index].geometry.coordinates, // geometry.coordinates
+                      hostId : u._id,
+                      device: resp.results[index].device,
+                      detector: resp.results[index].detector,
+                      tsPeriodEnd: resp.results[index].tsPeriodEnd,
+                      redPeriod: resp.results[index].redPeriod,
+                      queueLength: resp.results[index].queueLength,
+                      unitQueueLength: resp.results[index].unitQueueLength,
+                      vehicleCount: resp.results[index].vehicleCount,
+                      unitVehicleCount: resp.results[index].unitVehicleCount,
+                      maxWaitTime: resp.results[index].maxWaitTime,
+                      avgWaitTime: resp.results[index].avgWaitTime,
+                      unitWaitTime: resp.results[index].unitWaitTime,
+                      reliabValue: resp.results[index].reliabValue,
+                      unitReliab: resp.results[index].unitReliab
+                    });
+                  }
+                  catch(err) {
+                      console.log(resp.features[index]);
+                  }
+                  finally{
+                    Congestion.findOneAndUpdate({hostId: u._id}, {$set:newItem}, {new: true}, function(err, doc){
+                      if(err){
+
+                      }else{
+                        console.log("paivitetty")
+                        men = true;
+                      }
+
+                      });
+                      if (men == false){
+                        newItem.save(function(err, fluffy) {
+                          if (err) return console.error(err);
+                          console.log("sin men iha uus");
+
+                        });
+                      }
+
+
+                  }
+              }
+              }
+              catch(err) {
+                  console.log(err)
+                  console.log("valivaliälähaedataa");
+              }
+
+      });
+
+
+
+      }).on('error', function(e){
+            console.log("Got an error: ", e);
+      });
+
+
+      donearray.push(u.device)
+
+    }
+
+  });
+res.send("Miksi Tama tulee ennenku on valmista?")
+
+});
+
+
+
+
+
 
 app.get('/haeValot', function(req, res) {
 
   var request = require("request")
 
   var url = "http://opendata.navici.com/tampere/opendata/ows?service=WFS&version=2.0.0&request=GetFeature&typeName=opendata:WFS_LIIKENNEVALO_ILMAISIN&outputFormat=json&srsName=EPSG:4326"
-
 
 http.get(url, function(rs){
     var body = '';
@@ -140,9 +284,9 @@ http.get(url, function(rs){
           try {
             var newItem = new Light({
               coordinates:  resp.features[index].geometry.coordinates, // geometry.coordinates
-              laite: "tre"+resp.features[index].properties.LIITTYMAN_NRO, // "tre" + properties.ILMAISIN_ID
-              etaisyys: resp.features[index].properties.ETAISYYS, // properties.ETAISYYS
-              tunnus: resp.features[index].properties.TUNNUS
+              device: "tre"+resp.features[index].properties.LIITTYMAN_NRO, // "tre" + properties.ILMAISIN_ID
+              distance: resp.features[index].properties.ETAISYYS, // properties.ETAISYYS
+              detector: resp.features[index].properties.TUNNUS
             });
           }
           catch(err) {
@@ -152,7 +296,7 @@ http.get(url, function(rs){
 
             newItem.save(function(err, fluffy) {
               if (err) return console.error(err);
-              console.log("sin men");
+              // console.log("sin men");
             });
           }
 
@@ -164,10 +308,6 @@ http.get(url, function(rs){
 }).on('error', function(e){
       console.log("Got an error: ", e);
 });
-
-
-
-
 });
 
 
